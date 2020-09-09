76510220daff4174f72e68f1647fa7ed62885614
huihui <1870194282@qq.com>
2020-09-09 18:54:28 +0800

    修改FirstClass

--- git_test/src/tompeixun/FirstClass.java
+++ git_test/src/tompeixun/FirstClass.java
@@ -6,11 +6,13 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("测试FirstClass");
+        System.out.println("测试");
 
     }
     public void test(){
         int i=10;
         i+=10;
+        i=i+10;
         System.out.println(i);
     }
 }
